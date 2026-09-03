c2e3e07fa4bedd08501e761ac28e55a8767188dd
Lionn Man <84295076+lionnman@users.noreply.github.com>
2026-09-03 17:14:55 -0300

    Create registration.php

---
+++ projects/ mage2bar/magento2 /app/i18n/EnglishTranslation/en_US/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magento_englishtranslation_en_us',
+    __DIR__
+);
