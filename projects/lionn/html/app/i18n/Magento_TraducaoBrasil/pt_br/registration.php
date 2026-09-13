d4ca771f963db9eb7b7eec936cdabefb87b393ed
Lionn Man <84295076+lionnman@users.noreply.github.com>
2026-09-13 19:17:38 -0300

    Update registration.php

--- projects/lionn/html/app/i18n/Magento_TraducaoBrasil/pt_br/registration.php
+++ projects/lionn/html/app/i18n/Magento_TraducaoBrasil/pt_br/registration.php
@@ -1,6 +1,23 @@
 <?php
+
+/**
+ * Package: magento_traducaobrasil_pt_br
+ * [ Pacote: magento_traducaobrasil_pt_br ]
+ *
+ * Brazilian Portuguese (pt_BR) translation package for Magento 2.
+ * [ Pacote de tradução para Português do Brasil (pt_BR) do Magento 2. ]
+ *
+ * © Lionn - https://www.lionn.net
+ * [ © Lionn - https://www.lionn.net ]
+ *
+ * Licensed under the MIT License.
+ * [ Licenciado sob a Licença MIT. ]
+ * See LICENSE file for details.
+ * [ Veja o arquivo LICENSE para detalhes. ]
+ */
+
 \Magento\Framework\Component\ComponentRegistrar::register(
-\Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-'magento_traducaobrasil_pt_br',
-__DIR__
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magento_traducaobrasil_pt_br',
+    __DIR__
 );
