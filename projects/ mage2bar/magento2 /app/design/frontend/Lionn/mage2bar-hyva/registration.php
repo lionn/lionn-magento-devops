7c4f5ac207c4f7783988c175da1623f16ce568b3
Lionn Man <84295076+lionnman@users.noreply.github.com>
2026-09-25 20:00:49 -0300

    Delete projects/ mage2bar/magento2  directory

--- projects/ mage2bar/magento2 /app/design/frontend/Lionn/mage2bar-hyva/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::THEME,
-    'frontend/Lionn/mage2bar-hyva',
-    __DIR__
-);
--- projects/ mage2bar/magento2 /app/design/frontend/Lionn/mage2bar/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Lionn/mage2bar',
-    __DIR__
-);
